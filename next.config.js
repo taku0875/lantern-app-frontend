6d89298d85c41657ff5aa9fb0060c1af329f3c7b
taku0875 <always092471@gmail.com>
2025-08-15 11:54:03 +0900

    no message

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-require('dotenv').config()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        // Reference a variable that was defined in the .env file and make it available at Build Time
-        API_ENDPOINT: process.env.API_ENDPOINT,
-      },
-};
+    output: 'standalone'
+      }
 
-module.exports = nextConfig;
+module.exports = nextConfig
